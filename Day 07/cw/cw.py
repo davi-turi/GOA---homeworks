83b9df30a41dcf097cb1f2603927d51e29684d59
davi-turi <ninodavituri45@gmail.com>
2025-11-21 19:16:41 +0400

    eighth commit

---
+++ Day 07/cw/cw.py
@@ -0,0 +1,32 @@
+# input არის შემავალი ინფორმაცია,ხოლო output არის შედეგი.
+
+# INPUT
+input1="scene perceived by the camera"
+print(input)
+input2="photo that's sent to the printer"
+print(input2)
+
+# OUTPUT
+output="scene that we see from the camera"
+print(output)
+output2="result that's shown in terminal"
+print(output2)
+
+food = input("Enter your favourite food:")
+print(food)
+
+name= input("Enter your name:")
+print("Hello",name)
+
+num="28"
+number= input("Enter number:")
+print(int(num)+int(number))
+
+
+# var = input("enter your age")
+# print(var + 3)
+# ერორია, რადგან var ცვლადი წარმოდგენილია სტრინგად, სტრინგს კი ინტეჯერს ვერ დავუმატებთ.
+
+Number1=input("Choose a number:")
+Number2=input("Choose another number:")
+print(int(Number1)//int(Number2))
---
+++ Day 07/hw/hw.py
@@ -0,0 +1,18 @@
+age=input("Enter your age:")
+print(int(age)//10)
+
+num1=input("Enter a number:")
+num2=input("Enter another nomber:")
+print(int(num1)*int(num2))
+
+Age=input("Enter Your Age:")
+print(int(Age))
+
+Num=int(input("Enter One Number:"))
+Num1=int(input("Enter Another Number:"))
+Num2=int(input("Enter Another One:"))
+avarage=(Num+Num1+Num2)/3
+print(avarage)
+
+year=int(input("Enter your birth year"))
+print(2025-year)
