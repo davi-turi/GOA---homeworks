f97b5e15c44fbd17863cfaaffa4acfa6e747ae6f
davi-turi <ninodavituri45@gmail.com>
2025-11-26 20:25:53 +0400

    Update hw.py

---
+++ Day 05/hw/hw.py
@@ -0,0 +1,25 @@
+1.
+#debugging ნიშნავს შეცდომების გასწორებას.
+
+2.
+name=13
+name="Nini"
+print(name)
+
+3.
+first_name="Nini"
+last_name="Davituri"
+print(first_name + last_name)
+
+4.
+print(4564 % 4)
+print(9768 % 3)
+print(16785 % 3)
+
+5.
+name="saba"
+name = "giorgi"
+print(name)
+
+num=40
+print(num * 3)
